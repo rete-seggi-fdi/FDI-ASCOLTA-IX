c40c325597f79213f56ae704dd223277f48e8277
rete-seggi-fdi <coixmun@gmail.com>
2026-07-09 11:49:19 +0200

    Update API_URL in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,3 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz-Hh6unv7HQu6t-xuIaWNYbgedxsH3ofiMgq7OIC4FrymsFYx9HanHnpJ0A5N_eyBQBA/exec",
-  SESSION_KEY: "fdi_ascolta_ix_user"
+  API_URL: "https://script.google.com/macros/s/AKfycbzLJQ3oAfOcFTA2gMfNM6U3MmcZc1HJxZUF0Yu7AuPyVPrtDfCRA-jUyx2OkiEMEbq_oA/exec"
 };
